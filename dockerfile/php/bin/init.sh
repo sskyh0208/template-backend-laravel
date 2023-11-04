914073b425af1a0aa312c7a5cdc0bd982b5f6d5e
Yoshihiro Sasaki <ysasaki@bltinc.co.jp>
2023-11-04 23:43:09 +0900

    srcを除外に追加

--- dockerfile/php/bin/init.sh
+++ dockerfile/php/bin/init.sh
@@ -3,11 +3,15 @@
 cd /var/www/html
 
 if [ -d "/var/www/html" ] && [ "$(ls -A /var/www/html)" ]; then
+  printenv
   npm install
   composer install
-  chown -R www-data:www-data /var/www/html/storage
+  chmod -R 777 /var/www/html/storage
+  chown -R nginx:nginx /var/www/html/storage
+  php artisan route:clear
+  php artisan cache:clear
+  php artisan config:clear
+  php artisan migrate --force
 fi
-
-cd _
 
 exec php-fpm
